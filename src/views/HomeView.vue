c46e10639282024eed352614c750084670c4e638
fellowbeard <virgovibe@gmail.com>
2022-11-25 12:14:46 -0600

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,160 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <main role="main">
+      <section class="mt-4 mb-5">
+        <!-- <div class="container mb-4">
+          <h1 class="font-weight-bold title">Explore</h1>
+        </div> -->
+        <div class="container-fluid">
+          <div class="row">
+            <div class="card-columns">
+              <div class="card card-pin">
+                <video width="290" height="290" autoplay loop>
+                  <source src="/docs/assets/img/memphistwirl.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+
+                <div class="overlay">
+                  <h2 class="card-title title">Cool Title</h2>
+                  <div class="more">
+                    <a href="post.html">
+                      <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="card card-pin">
+                <img
+                  class="card-img"
+                  src="https://images.unsplash.com/photo-1489743342057-3448cc7c3bb9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6d284a2efbca5f89528546307f7e7b87&auto=format&fit=crop&w=500&q=60"
+                  alt="Card image"
+                />
+                <div class="overlay">
+                  <h2 class="card-title title">Cool Title</h2>
+                  <div class="more">
+                    <a href="post.html">
+                      <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card card-pin">
+                <img
+                  class="card-img"
+                  src="https://images.unsplash.com/photo-1509885903707-b68568db61ed?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5f11503655b51165836c2dcefa51a040&auto=format&fit=crop&w=500&q=60"
+                  alt="Card image"
+                />
+                <div class="overlay">
+                  <h2 class="card-title title">Cool Title</h2>
+                  <div class="more">
+                    <a href="post.html">
+                      <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="card card-pin">
+                <img
+                  class="card-img"
+                  src="https://images.unsplash.com/photo-1485627658391-1365e4e0dbfe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=976b0db5c3c2b9932bb20e72f98f9b61&auto=format&fit=crop&w=500&q=60"
+                  alt="Card image"
+                />
+                <div class="overlay">
+                  <h2 class="card-title title">Cool Title</h2>
+                  <div class="more">
+                    <a href="post.html">
+                      <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card card-pin">
+                <img
+                  class="card-img"
+                  src="https://images.unsplash.com/photo-1466692476868-aef1dfb1e735?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9e3cd6ce6496c9c05cbf1b6cda8be0f9&auto=format&fit=crop&w=500&q=60"
+                  alt="Card image"
+                />
+                <div class="overlay">
+                  <h2 class="card-title title">Cool Title</h2>
+                  <div class="more">
+                    <a href="post.html">
+                      <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="card card-pin">
+                <img
+                  class="card-img"
+                  src="https://images.unsplash.com/photo-1518707399486-6d702a84ff00?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b5bb16fa7eaed1a1ed47614488f7588d&auto=format&fit=crop&w=500&q=60"
+                  alt="Card image"
+                />
+                <div class="overlay">
+                  <h2 class="card-title title">Cool Title</h2>
+                  <div class="more">
+                    <a href="post.html">
+                      <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card card-pin">
+                <img
+                  class="card-img"
+                  src="https://images.unsplash.com/photo-1506706435692-290e0c5b4505?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0bb464bb1ceea5155bc079c4f50bc36a&auto=format&fit=crop&w=500&q=60"
+                  alt="Card image"
+                />
+                <div class="overlay">
+                  <h2 class="card-title title">Cool Title</h2>
+                  <div class="more">
+                    <a href="post.html">
+                      <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="card card-pin">
+                <img
+                  class="card-img"
+                  src="https://images.unsplash.com/photo-1512355144108-e94a235b10af?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c622d56d975113a08c71c912618b5f83&auto=format&fit=crop&w=500&q=60"
+                  alt="Card image"
+                />
+                <div class="overlay">
+                  <h2 class="card-title title">Cool Title</h2>
+                  <div class="more">
+                    <a href="post.html">
+                      <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                      More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {};
   },
+  created: function () {},
+  methods: {},
 };
 </script>
