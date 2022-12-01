8e0605d78e6dd1ffe09a5be70a559121667960e6
fellowbeard <virgovibe@gmail.com>
2022-12-01 15:13:35 -0600

    adds yam1 files

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCg8iALBMwNSrXg0RnG8nCyKVq0_vrk2ok",
+  authDomain: "personalwebsite2022-75125.firebaseapp.com",
+  projectId: "personalwebsite2022-75125",
+  storageBucket: "personalwebsite2022-75125.appspot.com",
+  messagingSenderId: "617059198354",
+  appId: "1:617059198354:web:b8b0df07375dabc3298d4b",
+  measurementId: "G-Z4JLX1Z9BF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
