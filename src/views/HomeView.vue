baa4e32db26851975d98f699ef1efdc002f1d7f8
fellowbeard <virgovibe@gmail.com>
2022-12-03 08:34:44 -0600

    reverts video to former style adds webm source

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,17 +8,9 @@
               <div class="vid-size">
                 <a href="/post">
                   <div class="card-pin card-video">
-                    <video
-                      style="width: 100%; height: auto"
-                      ref="videoRef"
-                      src=""
-                      autoplay="autoplay"
-                      loop
-                      id="vid-size"
-                      muted
-                      type="video/mp4"
-                    >
-                      <!-- <source src="/docs/assets/img/memphisme.mp4" type="video/mp4" /> -->
+                    <video style="width: 100%; height: auto" autoplay="autoplay" loop id="vid-size" muted>
+                      <source src="/docs/assets/img/memphisme.webm" type="video/webm" />
+                      <source src="/docs/assets/img/memphisme.mp4" type="video/mp4" />
                       Your browser does not support the video tag.
                     </video>
 
@@ -104,9 +96,9 @@ export default {
   },
   created: function () {},
   methods: {},
-  mounted: function () {
-    this.$refs.videoRef.src = "/docs/assets/img/memphisme.mp4";
-    this.$refs.videoRef.play();
-  },
+  // mounted: function () {
+  //   this.$refs.videoRef.src = "/docs/assets/img/memphisme.mp4";
+  //   this.$refs.videoRef.play();
+  // },
 };
 </script>
